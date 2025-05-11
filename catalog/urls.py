a79bfad08c24e2e0d745e24b7cc6a7270ba7ae34
unknown <extapo@yandex>
2025-05-11 16:44:49 +0700

    fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from catalog import views
+from django.conf import settings
+from django.conf.urls.static import static
+from catalog.apps import CatalogConfig
 
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
+
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_details, name='product_details')
 ]
